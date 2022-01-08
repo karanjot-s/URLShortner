4550f25169ed5ffe6f74dff83ee06b9843635e0e
Karanjot Singh <66475280+karanjot-s@users.noreply.github.com>
2022-01-08 14:52:05 +0530

    created new design

--- server.js
+++ server.js
@@ -5,25 +5,39 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(
-  "mongodb://karanjot:karan24mongodb@urlshortner-shard-00-00.zijij.mongodb.net:27017,urlshortner-shard-00-01.zijij.mongodb.net:27017,urlshortner-shard-00-02.zijij.mongodb.net:27017/UrlShortner?ssl=true&replicaSet=atlas-z13bh5-shard-0&authSource=admin&retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect("mongodb://localhost:27017/urlShortner", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// mongoose.connect(
+//   "mongodb://karanjot:karan24mongodb@urlshortner-shard-00-00.zijij.mongodb.net:27017,urlshortner-shard-00-01.zijij.mongodb.net:27017,urlshortner-shard-00-02.zijij.mongodb.net:27017/UrlShortner?ssl=true&replicaSet=atlas-z13bh5-shard-0&authSource=admin&retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/all", async (req, res) => {
   const shortUrls = await ShortUrl.find();
-  res.render("index", { shortUrls: shortUrls });
+  res.render("all", { shortUrls: shortUrls });
+});
+
+app.get("/success/:shortUrl", (req, res) => {
+  res.render("success", { url: req.params.shortUrl });
 });
 
 app.post("/shorturls", async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl });
-  res.redirect("/");
+  const url = await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("success/" + url.short);
 });
 
 app.get("/:shortUrl", async (req, res) => {
